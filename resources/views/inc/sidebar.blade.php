6541a1ad4476881aafff15c52e5067d875c2ceb4
psnapps <moshe.balasiano@bynet.co.il>
2022-01-27 09:40:03 +0200

    sidebar background color

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -11,7 +11,7 @@
         <div> 
             <a href="#" class="nav_logo"> 
                 <i class='bx bx-layer nav_logo-icon'></i> 
-                <span class="nav_logo-name">PSN</span> 
+                <span class="nav_logo-name">Bynet ITaaS</span> 
             </a>
             <div class="nav_list"> 
                 <a href="#" class="nav_link active"> 
@@ -20,23 +20,23 @@
                 </a> 
                 <a href="#" class="nav_link"> 
                     <i class='bx bx-user nav_icon'></i> 
-                    <span class="nav_name">Users</span> 
+                    <span class="nav_name">Global Settings</span> 
                 </a> 
                 <a href="#" class="nav_link"> 
                     <i class='bx bx-message-square-detail nav_icon'></i> 
-                    <span class="nav_name">Messages</span> 
+                    <span class="nav_name">Solution Management</span> 
                 </a> 
                 <a href="#" class="nav_link"> 
                     <i class='bx bx-bookmark nav_icon'></i> 
-                    <span class="nav_name">Bookmark</span> 
+                    <span class="nav_name">Authentication</span> 
                 </a> 
                 <a href="#" class="nav_link"> 
                     <i class='bx bx-folder nav_icon'></i> 
-                    <span class="nav_name">Files</span> 
+                    <span class="nav_name">Solution Menu</span> 
                 </a> 
                 <a href="#" class="nav_link"> 
                     <i class='bx bx-bar-chart-alt-2 nav_icon'></i> 
-                    <span class="nav_name">Stats</span> 
+                    <span class="nav_name">Scheduled Tasks</span> 
                 </a> 
             </div>
         </div> 
